2ddbae9adbaae8575e1aa1f09a780862d653dbd7
Nikita Migushev <migushev@gmail.com>
2023-05-29 17:45:00 +0300

    3. GC. Out Of Mem. Тестовое задание. [#504790]

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -7,14 +7,18 @@ import ru.job4j.articles.model.Word;
 import ru.job4j.articles.service.generator.ArticleGenerator;
 import ru.job4j.articles.store.Store;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleArticleService implements ArticleService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleArticleService.class.getSimpleName());
 
     private final ArticleGenerator articleGenerator;
+
+    private final int batchSize = 10000;
+
+    private int articleCounter = 0;
 
     public SimpleArticleService(ArticleGenerator articleGenerator) {
         this.articleGenerator = articleGenerator;
@@ -24,10 +28,24 @@ public class SimpleArticleService implements ArticleService {
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
         LOGGER.info("Геренация статей в количестве {}", count);
         var words = wordStore.findAll();
-        var articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
-                .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
-                .mapToObj((x) -> articleGenerator.generate(words))
-                .collect(Collectors.toList());
-        articles.forEach(articleStore::save);
+        int lastBatchSize = count % batchSize == 0 ? batchSize : count % batchSize;
+        int batchesNumber = count / batchSize > 0 ? (int) Math.floor(count / batchSize) : 1;
+        for (int i = 0; i < batchesNumber; i++) {
+            boolean last = batchesNumber - i == 1 ? true : false;
+            int size = last ? lastBatchSize : batchSize;
+            List<Article> articles = generateBatch(words, size);
+            articles.forEach(articleStore::save);
+        }
+    }
+
+    private List<Article> generateBatch(List<Word> words, int size) {
+        List<Article> articles = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            LOGGER.info(String.format("Сгенерирована статья № %s", articleCounter));
+            Article article = articleGenerator.generate(words);
+            articles.add(article);
+            articleCounter++;
+        }
+        return articles;
     }
 }
